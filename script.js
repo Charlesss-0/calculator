8ccf7c20bf599502b07729d9e5b5155e3c7d6ab4
carlos505 <aragonaugusto24@gmail.com>
2023-07-04 11:43:45 -0600

    Add operator functions

--- script.js
+++ script.js
@@ -4,8 +4,45 @@ const btns = document.querySelectorAll('button')
 let calculation = []
 let accumulation
 
-function printBtns(btn) {
+function add(a, b) {
+    const sum = a + b
+    resultScreen.innerHTML = `
+        <span>
+            ${sum}
+        </span>
+    `
+}
+
+function subtract(a, b) {
+    const sum = a - b
+    resultScreen.innerHTML = `
+        <span>
+            ${sum}
+        </span>
+    `
+}
+
+function multiply(a, b) {
+    const sum = a * b
+    resultScreen.innerHTML = `
+        <span>
+            ${sum}
+        </span>
+    `
+}
+
+function division(a, b) {
+    const sum = a / b
+    resultScreen.innerHTML = `
+        <span>
+            ${sum}
+        </span>
+    `
+}
+
+function getNumberValues(btn, firstNum, secondNum) {
     const value = btn.innerText
+
     if (value === 'AC') {
         calculation = []
         resultScreen.innerHTML = `
@@ -14,40 +51,28 @@ function printBtns(btn) {
             </span>
         `
     } else if (value === '=') {
-        resultScreen.innerHTML = `
-            <span>
-                ${math.evaluate(accumulation)}
-            </span>
-        `
+        if (calculation.includes('+')) {
+            add(+firstNum, +secondNum)
+
+        } else if (calculation.includes('-')) {
+            subtract(calculation.join(''))
+
+        } else if (calculation.includes('*')) {
+            multiply()
+
+        } else if (calculation.includes('/')) {
+            division()
+        }
     } else {
         calculation.push(value)
         accumulation = calculation.join('')
         resultScreen.innerHTML = `
-            <span class="rmv">
+            <span>
                 ${accumulation}
             </span>
         `
+    
     }
 }
 
-btns.forEach(btn => btn.addEventListener('click', () => printBtns(btn)))
-
-const plusEl = document.querySelector('.plus')
-const subtractEl = document.querySelector('.subtract')
-const multiplyEl = document.querySelector('.multiply')
-const divisionEl = document.querySelector('.division')
-
-let operators = ['+', '-', '*', '/']
-
-function findValue() {
-    if ((operators.includes(resultScreen.innerText))) {
-        if (calculation.includes(operators -1)) {
-            calculation.splice(-1)
-        }
-    }
-}
-
-plusEl.addEventListener('click', findValue)
-subtractEl.addEventListener('click', findValue)
-multiplyEl.addEventListener('click', findValue)
-divisionEl.addEventListener('click', findValue)
+btns.forEach(btn => btn.addEventListener('click', () => getNumberValues(btn)))
